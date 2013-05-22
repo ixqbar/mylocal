8bbf1907c609f173901cf6192d4064c720fa37d6
venkman <ixqbar@gmail.com>
2013-05-22 10:45:04 +0800

    update

--- python/learn_watchdog.py
+++ python/learn_watchdog.py
@@ -4,14 +4,52 @@ import sys
 import time
 import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import LoggingEventHandler,FileSystemEventHandler
+import shutil
+
+class CustomEventHandler(FileSystemEventHandler):
+    def on_moved(self, event):
+        super(CustomEventHandler, self).on_moved(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Moved %s: from %s to %s", what, event.src_path,
+                     event.dest_path)
+
+    def on_created(self, event):
+        super(CustomEventHandler, self).on_created(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)
+
+        try:
+            shutil.copy(event.src_path, "/tmp/11.php")
+        except:
+            pass
+
+    def on_deleted(self, event):
+        super(CustomEventHandler, self).on_deleted(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Deleted %s: %s", what, event.src_path)
+
+    def on_modified(self, event):
+        super(CustomEventHandler, self).on_modified(event)
+
+        # what = 'directory' if event.is_directory else 'file'
+        # logging.info("Modified %s: %s", what, event.src_path)
+
+        try:
+            shutil.copy(event.src_path, "/tmp/1.php")
+        except:
+            pass
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    path = sys.argv[1] if len(sys.argv) > 1 else '/tmp/'
+    event_handler = CustomEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
