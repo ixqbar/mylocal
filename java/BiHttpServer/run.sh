2426f8f245b6ba2796b57ede7395be52806fc0c3
jonnywang <ixqbar@gmail.com>
2013-07-14 12:38:59 +0800

    update

--- java/BiHttpServer/run.sh
+++ java/BiHttpServer/run.sh
@@ -1,3 +1,17 @@
 #/bin/sh
 
-java -jar HttpServer.jar 10.10.41.120 8080 /home/venkman/workspace/BiHttpServer/biLogs /home/venkman/workspace/BiHttpServer/httpLogs 1 0 1 0
+#You can usage like java -jar HttpServer.jar Options
+#Options Detail:
+#	--address                 default 0.0.0.0
+#	--port                    default 8080
+#	--biLogPath               default /var/logs
+#	--httpLogPath             default /var/logs
+#	--biLogToCompress         like 0 or 1 default 1
+#	--httpLogTocompress       like 0 or 1 default 0
+#	--delBiLogAfterCompress   like 0 or 1 default 1
+#	--delHttpLogAfterCompress like 0 or 1 default 0
+#	--biPerLogMaxNum          default 10000
+#	--httpPerLogMaxNum        default 10000
+#	--serverLogToFile         default 0
+#	
+java -jar HttpServer.jar --address=127.0.0.1 --port=8080 --biLogPath=/Users/venkman/Desktop/HttpServer/logs --httpLogPath=/Users/venkman/Desktop/HttpServer/logs
