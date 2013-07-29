012dcaf95eb09d9a40dbbba4e00b9d483f44cde0
venkman <ixqbar@gmail.com>
2013-07-29 18:12:32 +0800

    del

--- java/BiHttpServer/src/com/shbk/bi/core/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.shbk.bi.core;
-
-import java.io.IOException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-
-public interface Handler {
-	void execute(Selector selector, SelectionKey key) throws IOException;  
-}
